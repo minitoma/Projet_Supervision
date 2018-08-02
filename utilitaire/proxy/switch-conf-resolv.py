c82fb4446d805ccfbeb8bbf77407b1b987528caf
minitoma <calatayud.thomas@hotmail.fr>
2018-08-02 16:00:25 +0200

    Ajout scripts, images, et tutos

---
+++ utilitaire/proxy/switch-conf-resolv.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+import sys
+import os
+
+def switchToSaeir():
+    os.rename("/etc/resolv.conf", "/etc/resolv.conf.cg49")
+    os.rename("/etc/resolv.conf.saeir", "/etc/resolv.conf")
+
+def switchToCG49():
+    os.rename("/etc/resolv.conf", "/etc/resolv.conf.saeir")
+    os.rename("/etc/resolv.conf.cg49", "/etc/resolv.conf")
+
+if __name__ ==  "__main__":
+    if len(sys.argv) == 2:
+        if '--help' ==  sys.argv[1]:
+            print("##### This script switch the resolv.conf file to perform to the saeir or cg49 domain #####")
+            print
+            print("You may need to use this script with sudo")
+            print("Usage :")
+            print("    --cg49 to switch resolv.conf for the cg49 domain")
+            print("    --saeir to switch resolv.conf for the saeir domain")
+            print("    --help to show this help")
+        elif '--cg49' == sys.argv[1]:
+            try:
+                if os.path.isfile("/etc/resolv.conf.cg49"):
+                    switchToCG49()
+                    print("switching /etc/resolv.conf for the cg49 domain")
+                else:
+                    print("resolv.conf is already configure for the cg49 domain")
+            except:
+                print("*** WARNING ***")
+                print("You have no right to use this script !")
+                print("Try to use sudo !")
+        elif '--saeir' == sys.argv[1]:
+            try:
+                if os.path.isfile("/etc/resolv.conf.saeir"):
+                    switchToSaeir()
+                    print("switching /etc/resolv.conf for the saeir domain")
+                else:
+                    print("resolv.conf is already configure for the seair domain")
+            except:
+                print("*** WARNING ***")
+                print("You have no right to use this script !")
+                print("Try to use sudo !")
+        else:
+            print("Unknown argument, --help for more help")
+    else:
+        print("Unknown command or missing arguments, --help for more help")
+        sys.exit(2)
+        sys.exit(0)
+else:
+    print("Help with --help" % sys.argv[0])
+    sys.exit(2)
---
+++ utilitaire/templates/replace.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import sys
+import xml.etree.ElementTree as ET
+
+def searchTag(tag,fileToOpen,message):
+    try:
+        tree = ET.parse(fileToOpen)
+        root = tree.getroot()
+        print(tree)
+        print(root)
+        for tag in root.iter(tag):
+            tag.text = str(message)
+            print(tag.text)
+        tree.write(fileToOpen)
+    except:
+        print("** ERROR !!! **")
+        print(fileToOpen+" is not a valid xml file")
+
+if __name__ ==  "__main__":
+    if len(sys.argv) == 2:
+        if '--help' ==  sys.argv[1]:
+            print("##### This script is using to replace a text from a specific tag in a xml file #####")
+            print
+            print("Usage (arguments must be in this order):")
+            print("    -f <xml file to modify>")
+            print("    -t <tag to search> ; for exemple : trends or history")
+            print("    -r <text to replace> ; this text would be between the tag you search with the -t argument")
+    elif len(sys.argv) == 7:
+        if '-f' == sys.argv[1] and \
+           '-t' == sys.argv[3] and \
+           '-r' == sys.argv[5]:
+                fileToOpen = sys.argv[2]
+                tag = sys.argv[4]
+                message = sys.argv[6]
+                searchTag(tag,fileToOpen,message)
+        else:
+            print("Arguments are not in the right order, --help for more help")
+    else:
+        print("Unknown command or missing arguments, --help for more help")
+        sys.exit(2)
+        sys.exit(0)
+else:
+    print("Help with --help" % sys.argv[0])
+    sys.exit(2)
